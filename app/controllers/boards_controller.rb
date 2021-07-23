e87108b4ee43a46bfdbc399a2e2622a80ce072e3
Eric Ma <phatbhuda@gmail.com>
2021-07-23 16:30:19 -0400

    slideshow

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -2,4 +2,48 @@ class BoardsController < ApplicationController
   def index
     @boards = Board.all
   end
+
+  def show
+    @board = Board.find(params[:id])
+  end
+
+  def new
+    @board = Board.new
+  end
+
+  def create
+    @board = Board.new(board_params)
+
+    if @board.save
+      redirect_to @board
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @board = Board.find(params[:id])
+  end
+
+  def update
+    @board = Board.find(params[:id])
+
+    if @board.update(board_params)
+      redirect_to @board
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @board = Board.find(find(params[:id]))
+    @board.destroy
+
+    redirect_to root_path
+  end
+
+  private
+  def board_params
+    params.require(:board).permit(:title, :url)
+  end
 end
--- app/models/board.rb
+++ app/models/board.rb
@@ -1,2 +1,4 @@
 class Board < ApplicationRecord
+  validates :title, presence: true
+  validates :url, presence: true, length: {minimum: 10}
 end
